da1cf0d04eeec02dde4070d04e621cf0f01e31c2
dhanush251201 <20z214@psgtech.ac.in>
2023-02-17 09:26:30 +0530

    17-2-23

--- frontend/college-club-directory/src/pages/ClubDetails.js
+++ frontend/college-club-directory/src/pages/ClubDetails.js
@@ -26,14 +26,17 @@ const ClubDetails = () => {
                 <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
                     <div className="flex flex-row justify-between items-center w-full">
                         <p className="text-4xl font-bold">{club.name}</p>
-                        <div className="flex flex-row w-fit justify-between">
-                            <p className="text-2xl mb-4 bg-green-500 rounded-full px-3 text-white">{club.type}</p>
+                        <div className="flex flex-row w-fit m-10 justify-between">
+                            <p className="text-2xl mb-4 bg-green-500 rounded-full px-3 mr-4 text-white">{club.type}</p>
                             <p className="text-2xl mb-4 bg-violet-500 rounded-full px-3 text-white">{club.dept}</p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <p>{club.description}</p>
+                    <div className="flex flex-row justify-between items-center">
+                        <p className="w-1/2 text-right h-full p-5">{club.description}</p>
+                        <img src={club.imageLg} className="w-1/3 rounded-t-lg rounded-tl-[50px] rounded-b-lg rounded-br-[50px]"/>
+                    </div>
                     <p>{club.recruitingYear} onwards</p>
                     <p>Faculty incharge : {club.incharge}</p>
                     <p>Student Secretary :{club.secretary}</p>
